05c96318a667ca46613299960fcd3ae7fe0d5db1
programmerrams <beyramses@gmail.com>
2025-02-17 19:16:47 -0500

    1. accounts.html added for bank ammounts 2. registration.js added for registration page 3. accounts.css added for accounts page 4. html files will have to a form so data can be validated.

---
+++ JS/registration.js
@@ -0,0 +1,11 @@
+// defining variables 
+
+let firstName = document.querySelector(".first-name-box"); 
+
+let lastName = document.querySelector(".last-name-box");
+
+let username = document.querySelector(".username-box");
+
+let email = document.querySelector(".email-box");
+
+console.log(username);
