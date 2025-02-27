a9e0ffec68eb2d56e6a69b7f7c66e484fed42f45
programmerrams <beyramses@gmail.com>
2025-02-27 08:27:46 -0500

    rates page added, css added, and creditResults.js modified

--- JS/creditResults.js
+++ JS/creditResults.js
@@ -7,6 +7,13 @@ let bankStatement = document.querySelector(".credit-statement");
 let userFirstName = localStorage.getItem("firstName");
 let userLastName = localStorage.getItem("lastName");  
 let creditLimitAmount = localStorage.getItem("creditAmount");
-// let approvedAmount = localStorage.getItem("creditAmount");
+let creditScore = localStorage.getItem("creditScoreValue");
 
+let creditscore = Number(localStorage.getItem("Credit Score"));
+
+
+if (creditscore >= 600) {
 bankStatement.textContent = `hello ${userFirstName} ${userLastName}, after careful consideration your credit card limit is $${creditLimitAmount}.`;
+}else {
+    bankStatement.textContent = `hello ${userFirstName} ${userLastName}, after careful consideration you are not qualified for a credit card.`;
+}
