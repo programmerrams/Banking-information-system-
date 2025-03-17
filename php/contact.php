00d627d7a660e057582a9f519bcd444a04932770
Jeremy <exertible@gmail.com>
2025-03-17 10:11:12 -0700

    Add contact form and processing script: create HTML form for user inquiries and PHP script to handle form submissions. (testing sql and php)

---
+++ php/contact.php
@@ -0,0 +1,5 @@
+$con = mysqli_connect("localhost", "root", "","FlagBank", "FlagBank");
+$txtName = $_POST['txtName'];
+$txtEmail = $_POST['txtEmail'];
+$txtPhone = $_POST['txtPhone'];
+$txtMessage = $_POST['txtMessage'];
