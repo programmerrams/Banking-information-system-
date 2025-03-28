3f05f01c8c8dea976bb84dd4c3e2b0e91c7ccaf5
Jeremy <exertible@gmail.com>
2025-03-28 10:04:00 -0400

    t

--- php/database_connection.php
+++ php/database_connection.php
@@ -5,12 +5,13 @@
     $db_name = "flagbank";
     $conn = "";
 
-    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
-
+    try {
+        $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    }
+    catch (mysqli_sql_exception){
+        echo"Could not connect!";
+    }
     if ($conn){
         echo"You are connected.";
-    }
-    else {
-        echo "You are NOT connected.";
     }
 ?>
