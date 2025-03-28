351eba2f76e59acfaca7ac0f73d692f4b2f6bf2d
Jeremy <exertible@gmail.com>
2025-03-28 10:29:29 -0400

    testin

--- php/contact.php
+++
@@ -1,5 +0,0 @@
-$con = mysqli_connect("localhost", "root", "","FlagBank", "FlagBank");
-$txtName = $_POST['txtName'];
-$txtEmail = $_POST['txtEmail'];
-$txtPhone = $_POST['txtPhone'];
-$txtMessage = $_POST['txtMessage'];
