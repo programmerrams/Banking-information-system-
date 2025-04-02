561d83da5042c8757697139114baba1971f83968
Jeremy <jeremymohankalawan@gmail.com>
2025-04-01 22:07:32 -0400

    Refactor database connection error handling and improve HTML structure with consistent formatting

--- php/database_connection.php
+++ php/database_connection.php
@@ -3,15 +3,16 @@
     $db_user = "root";
     $db_pass = "";
     $db_name = "flagbank";
-    $conn = "";
+    $conn = null;
 
     try {
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
     }
-    catch (mysqli_sql_exception){
-        echo"Could not connect!";
+    catch (mysqli_sql_exception $e){
+        echo "Could not connect! Error: " . $e->getMessage();
     }
     if ($conn){
-        echo"You are connected.";
-    }
+        echo "<p>You are connected.</p>";
+  }
 ?>
