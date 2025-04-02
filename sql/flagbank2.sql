c3d8370665b7aa827c721131e020af3a421724c7
programmerrams <beyramses@gmail.com>
2025-04-02 14:36:01 -0400

    script for flagbank2 (sql)

---
+++ sql/flagbank2.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE flagbank2;
+
+USE flagbank2;
+
+create table creditcards (
+  first_name varchar(255),
+  last_name varchar(255),
+  credit_score int(255),
+  annual_salary int(255)
+);
