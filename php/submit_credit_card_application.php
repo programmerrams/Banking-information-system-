a039278d138d674be78df4b4eefef1b774d0a6da
Jeremy <jeremymohankalawan@gmail.com>
2025-04-01 22:31:23 -0400

    Refactor credit card application submission to use prepared statements and improve error handling

--- php/submit_credit_card_application.php
+++ php/submit_credit_card_application.php
@@ -1,32 +1,33 @@
 <?php
 include("database_connection.php");
 
-if(isset($_POST['submit'])) {
-    // Correcting variable names to match form input names
+if (isset($_POST['submit'])) {
+    // Retrieving form data
     $first_name = $_POST['formFirstName'];
     $last_name = $_POST['formLastName'];
     $credit_score = $_POST['formCreditScore'];
     $annual_salary = $_POST['formAnnualSalary'];
 
-    // Fixing the SQL query syntax
+    // Using prepared statements to prevent SQL injection
     $query = "INSERT INTO credit_cards (first_name, last_name, credit_score, annual_salary) 
-              VALUES ('$first_name', '$last_name', '$credit_score', '$annual_salary')";
+              VALUES (?, ?, ?, ?)";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "ssis", $first_name, $last_name, $credit_score, $annual_salary);
 
-    $result = mysqli_query($conn, $query);
-
-    if($result) {
+    if (mysqli_stmt_execute($stmt)) {
         echo "<script>alert('Data inserted successfully')</script>";
     } else {
         echo "<script>alert('There was an error: " . mysqli_error($conn) . "')</script>";
     }
+
+    mysqli_stmt_close($stmt);
 }
 
-// Fetching data from the flagbank table to display submitted data
-$sql = "SELECT * FROM credit_cards;";
+// Fetching data from the credit_cards table to display submitted data
+$sql = "SELECT * FROM credit_cards";
 $result = mysqli_query($conn, $sql);
-$resultCheck = mysqli_num_rows($result);
 
-if ($resultCheck > 0) {
+if (mysqli_num_rows($result) > 0) {
     echo "<h2>Submitted Data:</h2>";
     echo "<table border='1'>
             <tr>
@@ -44,5 +45,9 @@ if ($resultCheck > 0) {
               </tr>";
     }
     echo "</table>";
+} else {
+    echo "<h2>No data found</h2>";
 }
+
+mysqli_close($conn);
 ?>
