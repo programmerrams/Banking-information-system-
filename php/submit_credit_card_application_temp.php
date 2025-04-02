35a93a4d86123b25206c7fc115b4eeee2e92c321
Jeremy <exertible@gmail.com>
2025-04-02 13:23:08 -0400

    Refactor credit card application form and submission script for improved structure and data handling

--- php/submit_credit_card_application_temp.php
+++ php/submit_credit_card_application_temp.php
@@ -14,27 +14,18 @@ $password = "";
 $dbname = "flagbank";
 
 // Create connection
-$conn = mysqli_connect($host, $user, $password, $dbname);
+$conn = mysqli_connect("localhost", "root", "", "flagbank");
 
-if (mysqli_connect_errno()) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-
-$sql = "INSERT INTO credit_cards (first_name, last_name, credit_score, annual_salary) 
-VALUES (?, ?, ?, ?);";
-
-if (!($stmt = mysqli_prepare($conn, $sql))) {
-    die(mysqli_error($conn));
+if (isset($_POST['submit'])) {
+    // Retrieving form data
+    $first_name = $_POST["formFirstName"];
+    $last_name = $_POST["formLastName"];
+    $credit_score = $_POST["formCreditScore"];
+    $annual_salary = $_POST["formAnnualSalary"];
 }
 
-mysqli_stmt_bind_param($stmt, "ssii", $first_name, $last_name, $credit_score, $annual_salary);
+$query = "INSERT INTO 'credit_cards' VALUES ('$first_name', '$last_name', '$credit_score', '$annual_salary')";
 
-if (mysqli_stmt_execute($stmt)); {
-    echo "New record created successfully";
-    echo $last_name;
-    echo $first_name;
-    echo $credit_score;
-    echo $annual_salary;
-}
-    
+mysqli_query($conn, $query);
+
+?>
