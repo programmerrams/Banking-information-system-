fcc40c1836a9cc9029e873e5729c3e93c4f9df9b
programmerrams <beyramses@gmail.com>
2025-04-20 19:43:32 -0400

    bugfix: th gear button now opens only if the correct password is entered.

--- JS/rate and fee.js
+++ JS/rate and fee.js
@@ -5,25 +5,18 @@ console.log(gearSymbolDOM);
 let password = "Admin123";
 
 // requires a password if the user clicks on the gear button
-gearSymbolDOM.addEventListener("click", function () {
-  
-  
-  
+gearSymbolDOM.addEventListener("click", function (event) {
+  event.preventDefault();
   let userPassWord = prompt("Please enter the password to access settings:");
-  
-  while (!userPassWord === password) {
-    // prompt the user for a password
- 
-    // check if the password is correct
-    if (userPassWord === password) {
-      // if correct, show the settings
-      alert("Access granted! You can now change the settings.");
-      // Here you can add code to show the settings
-    } else {
-      // if incorrect, show an error message
-      alert("Access denied! Incorrect password.");
-      //refreshs the page
-      location.reload();
-    }
+
+  // prompt the user for a password
+  if (userPassWord != password) {
+    alert("Access denied! Incorrect password.");
+  }
+  // check if the password is correct
+  else {
+    // if correct, show the settings
+    window.location.href = "changeRates.html";
+    // alert("Access granted! You can now change the settings.");
   }
 });
